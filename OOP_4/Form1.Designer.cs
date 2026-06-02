ca1502806a554bcba3ea6509230fbc55fd61320e
birce02 <145684726+birce02@users.noreply.github.com>
2026-06-02 20:15:01 +0300

    Add files via upload

---
+++ OOP_4/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace InClass_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNameSurname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lstModels = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.cmbCity = new System.Windows.Forms.ComboBox();
+            this.cmbDistrict = new System.Windows.Forms.ComboBox();
+            this.btnCreateOrder = new System.Windows.Forms.Button();
+            this.lstOrders = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grpSizes = new System.Windows.Forms.GroupBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.grpSizes.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name-Surname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone Number";
+            // 
+            // txtNameSurname
+            // 
+            this.txtNameSurname.Location = new System.Drawing.Point(138, 21);
+            this.txtNameSurname.Name = "txtNameSurname";
+            this.txtNameSurname.Size = new System.Drawing.Size(100, 22);
+            this.txtNameSurname.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(138, 59);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 22);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // lstModels
+            // 
+            this.lstModels.FormattingEnabled = true;
+            this.lstModels.ItemHeight = 16;
+            this.lstModels.Items.AddRange(new object[] {
+            "Nike Air Max 90",
+            "Nike Air Max 91",
+            "Nike Air Max 41",
+            "Nike Air Max 60"});
+            this.lstModels.Location = new System.Drawing.Point(20, 42);
+            this.lstModels.Name = "lstModels";
+            this.lstModels.Size = new System.Drawing.Size(235, 132);
+            this.lstModels.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(43, 20);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "38";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(108, 18);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(43, 20);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "39";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(188, 18);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(43, 20);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "40";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(283, 18);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(43, 20);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "41";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(384, 21);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(43, 20);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Text = "42";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(23, 71);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(43, 20);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.Text = "43";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(108, 71);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(43, 20);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Text = "44";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // cmbCity
+            // 
+            this.cmbCity.FormattingEnabled = true;
+            this.cmbCity.Items.AddRange(new object[] {
+            "Eskişehir",
+            "Ankara"});
+            this.cmbCity.Location = new System.Drawing.Point(87, 64);
+            this.cmbCity.Name = "cmbCity";
+            this.cmbCity.Size = new System.Drawing.Size(121, 24);
+            this.cmbCity.TabIndex = 15;
+            this.cmbCity.SelectedIndexChanged += new System.EventHandler(this.cmbCity_SelectedIndexChanged);
+            // 
+            // cmbDistrict
+            // 
+            this.cmbDistrict.FormattingEnabled = true;
+            this.cmbDistrict.Location = new System.Drawing.Point(279, 64);
+            this.cmbDistrict.Name = "cmbDistrict";
+            this.cmbDistrict.Size = new System.Drawing.Size(121, 24);
+            this.cmbDistrict.TabIndex = 16;
+            // 
+            // btnCreateOrder
+            // 
+            this.btnCreateOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCreateOrder.Image = ((System.Drawing.Image)(resources.GetObject("btnCreateOrder.Image")));
+            this.btnCreateOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCreateOrder.Location = new System.Drawing.Point(128, 109);
+            this.btnCreateOrder.Name = "btnCreateOrder";
+            this.btnCreateOrder.Size = new System.Drawing.Size(195, 65);
+            this.btnCreateOrder.TabIndex = 17;
+            this.btnCreateOrder.Text = "Create Order";
+            this.btnCreateOrder.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCreateOrder.UseVisualStyleBackColor = true;
+            this.btnCreateOrder.Click += new System.EventHandler(this.btnCreateOrder_Click);
+            // 
+            // lstOrders
+            // 
+            this.lstOrders.FormattingEnabled = true;
+            this.lstOrders.ItemHeight = 16;
+            this.lstOrders.Location = new System.Drawing.Point(20, 31);
+            this.lstOrders.Name = "lstOrders";
+            this.lstOrders.Size = new System.Drawing.Size(774, 84);
+            this.lstOrders.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNameSurname);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(44, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 100);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer Information";
+            // 
+            // grpSizes
+            // 
+            this.grpSizes.Controls.Add(this.checkBox8);
+            this.grpSizes.Controls.Add(this.checkBox7);
+            this.grpSizes.Controls.Add(this.checkBox6);
+            this.grpSizes.Controls.Add(this.checkBox5);
+            this.grpSizes.Controls.Add(this.checkBox4);
+            this.grpSizes.Controls.Add(this.checkBox3);
+            this.grpSizes.Controls.Add(this.checkBox2);
+            this.grpSizes.Controls.Add(this.checkBox1);
+            this.grpSizes.Location = new System.Drawing.Point(361, 28);
+            this.grpSizes.Name = "grpSizes";
+            this.grpSizes.Size = new System.Drawing.Size(494, 117);
+            this.grpSizes.TabIndex = 20;
+            this.grpSizes.TabStop = false;
+            this.grpSizes.Text = "Shoe Size Selection";
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(188, 71);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(43, 20);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Text = "45";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lstOrders);
+            this.groupBox3.Location = new System.Drawing.Point(40, 398);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(814, 133);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Created Orders";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lstModels);
+            this.groupBox4.Location = new System.Drawing.Point(44, 169);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(275, 206);
+            this.groupBox4.TabIndex = 22;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Shoe Model Selection";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Controls.Add(this.btnCreateOrder);
+            this.groupBox5.Controls.Add(this.cmbDistrict);
+            this.groupBox5.Controls.Add(this.cmbCity);
+            this.groupBox5.Location = new System.Drawing.Point(364, 169);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(490, 206);
+            this.groupBox5.TabIndex = 23;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Shipping Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "District";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "City";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 558);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.grpSizes);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpSizes.ResumeLayout(false);
+            this.grpSizes.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNameSurname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.ListBox lstModels;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.ComboBox cmbCity;
+        private System.Windows.Forms.ComboBox cmbDistrict;
+        private System.Windows.Forms.Button btnCreateOrder;
+        private System.Windows.Forms.ListBox lstOrders;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grpSizes;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox8;
+    }
+}
+
---
+++ OOP_4/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace InClass_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCity.Items.Add("Ankara");
+            cmbCity.Items.Add("Eskişehir");
+        }
+
+        
+        private void cmbCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbDistrict.Items.Clear(); 
+
+            if (cmbCity.SelectedItem != null)
+            {
+                string secilenIl = cmbCity.SelectedItem.ToString();
+
+                if (secilenIl == "Ankara")
+                {
+                    cmbDistrict.Items.AddRange(new string[] { "Çankaya", "Keçiören", "Yenimahalle", "Polatlı", "Mamak", "Altındağ", "Pursaklar" });
+                }
+                else if (secilenIl == "Eskişehir")
+                {
+                    cmbDistrict.Items.AddRange(new string[] { "Odunpazarı", "Tepebaşı", "Sivrihisar", "İnönü", "Beylikova", "Çifteler" });
+                }
+
+               
+                if (cmbDistrict.Items.Count > 0)
+                    cmbDistrict.SelectedIndex = 0;
+            }
+        }
+
+        
+        private void btnCreateOrder_Click(object sender, EventArgs e)
+        {
+            
+            string isimSoyisim = txtNameSurname.Text.Trim();
+            string telefon = txtPhone.Text.Trim();
+
+            
+            if (isimSoyisim.Length < 3)
+            {
+                MessageBox.Show("Lütfen isim olarak en az 3 karakter giriniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            
+            if (telefon.Length < 10)
+            {
+                MessageBox.Show("Lütfen geçerli bir Telefon Numarası giriniz (En az 10 haneli olmalıdır).", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            
+            if (lstModels.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen Ayakkabı Modelini seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cmbCity.SelectedItem == null || cmbDistrict.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen Adresi eksiksiz doldurunuz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string secilenModel = lstModels.SelectedItem.ToString();
+
+            
+            List<string> secilenNumaralarListesi = new List<string>();
+            FindCheckedBoxes(this, secilenNumaralarListesi);
+
+            string secilenNumaralar = string.Join(", ", secilenNumaralarListesi);
+
+            
+            if (string.IsNullOrEmpty(secilenNumaralar))
+            {
+                MessageBox.Show("Lütfen en az bir ayakkabı numarası seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            
+            string il = cmbCity.SelectedItem.ToString();
+            string ilce = cmbDistrict.SelectedItem.ToString();
+
+            
+            string tarih = DateTime.Now.ToString("dd.MM.yyyy");
+
+            
+            string siparisOzeti = $"{isimSoyisim} | {telefon} | {secilenModel} | Sizes: {secilenNumaralar} | {il}/{ilce} | {tarih}";
+
+            lstOrders.Items.Add(siparisOzeti);
+        }
+        private void FindCheckedBoxes(Control parent, List<string> list)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is CheckBox chk && chk.Checked)
+                {
+                    list.Add(chk.Text);
+                }
+                else if (c.HasChildren)
+                {
+                    FindCheckedBoxes(c, list); 
+                }
+            }
+        }
+    }
+}
